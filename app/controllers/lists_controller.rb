89ec0e4841942f42a49fb69811e3aeefd585a35a
kaveesh sunassee <kaveesh.sunassee@gmail.com>
2021-07-30 18:07:12 +0400

    watch list01

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,46 +1,28 @@
 class ListsController < ApplicationController
-before_action :set_list, only: [:update, :show, :edit, :destroy]
+  def index
+    @lists = List.all
+  end
 
-	def index
-		@lists = List.all
-	end
+  def show
+    @list = List.find(params[:id])
+  end
 
-	def new
-		@list = List.new
-	end
+  def new
+    @list = List.new
+  end
 
-	def show
-	end
+  def create
+    @list = List.new(lists_params)
+    if @list.save
+      redirect_to list_path(@list)
+    else
+      render :new
+    end
+  end
 
-	def create
-		@list = List.new(list_params)
-		if @list.save
-			redirect_to list_path(@list)
-		else
-			render :new
-		end
-	end
+  private
 
-	# def update
-	# 	@list.update(list_params)
-	# 	redirect_to list_path(@list)
-	# end
-
-	# def edit
-	# end
-
-	def destroy
-		@list.destory
-	end
-
-
-private
-
-def list_params
-	params.require(:list).permit(:name)
-end
-
-def set_list
-	@list = List.find(params[:id])
-end
+  def lists_params
+    params.require(:list).permit(:name)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-root to: "lists#index"
-resources :lists, only: [:index, :show, :new, :create ] do 
-  resources :bookmarks, only: [:new, :create]
-end
+  get  '/lists',     to: 'lists#index'
+  get  '/lists/new', to: 'lists#new',  as: 'new_list'
+  get  '/lists/:id', to: 'lists#show', as: 'list'
+  post '/lists',     to: 'lists#create'
+
+  get    '/lists/:list_id/bookmarks/new', to: 'bookmarks#new', as: 'new_bookmark'
+  post   '/lists/:list_id/bookmarks',      to: 'bookmarks#create', as: 'list_bookmarks'
+  delete '/bookmarks/:id',            to: 'bookmarks#destroy', as: 'bookmark'
 end
