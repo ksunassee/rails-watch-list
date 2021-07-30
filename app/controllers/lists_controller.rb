c3eb37e1db9641cebddd236f90aa2a4fa3abc0d5
kaveesh sunassee <kaveesh.sunassee@gmail.com>
2021-07-31 00:02:56 +0400

    watch list 01

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,4 +1,6 @@
 class ListsController < ApplicationController
+before_action :set_list, only: [:show, :edit, :update, :destroy]
+
   def index
     @lists = List.all
   end
@@ -14,15 +16,25 @@ class ListsController < ApplicationController
   def create
     @list = List.new(lists_params)
     if @list.save
-      redirect_to list_path(@list)
+      redirect_to @list
     else
       render :new
     end
   end
 
+  def destroy
+  	@list.destroy
+  	redirect_to lists_path
+  end
+
   private
+
+  def set_list
+  	@list  = List.find(params[:id])
+  end
 
   def lists_params
     params.require(:list).permit(:name)
   end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,17 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get  '/lists',     to: 'lists#index'
-  get  '/lists/new', to: 'lists#new',  as: 'new_list'
-  get  '/lists/:id', to: 'lists#show', as: 'list'
-  post '/lists',     to: 'lists#create'
-
-  get    '/lists/:list_id/bookmarks/new', to: 'bookmarks#new', as: 'new_bookmark'
-  post   '/lists/:list_id/bookmarks',      to: 'bookmarks#create', as: 'list_bookmarks'
-  delete '/bookmarks/:id',            to: 'bookmarks#destroy', as: 'bookmark'
+  # get  '/lists',     to: 'lists#index'
+  # get  '/lists/new', to: 'lists#new',  as: 'new_list'
+  # get  '/lists/:id', to: 'lists#show', as: 'list'
+  # post '/lists',     to: 'lists#create'
+  # get    '/lists/:list_id/bookmarks/new', to: 'bookmarks#new', as: 'new_bookmark'
+  # post   '/lists/:list_id/bookmarks',      to: 'bookmarks#create', as: 'list_bookmarks'
+  # delete '/bookmarks/:id',            to: 'bookmarks#destroy', as: 'bookmark'
+root to: "lists#index"
+resources :lists do
+  resources :bookmarks 
 end
+
+end
+
+
